248f1f05fc8cfcf621e8678feb493d307cb5a87a
Engr-Jawad <jawadbuniri2@gmail.com>
2025-11-28 20:34:50 +0500

    list_sum

---
+++ list_element_sum.py
@@ -0,0 +1,8 @@
+# my_list=[1,2,3,4]
+my_list=int(input("enter elements in the list "))
+list=[]
+sum=0
+for i in range(len(list)):
+    sum+=list[i]
+print("sum of the element in the list : ",sum)
+    
