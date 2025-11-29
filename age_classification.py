3175bf4f3c919f87f0c628305698276bb1c4c9bf
Engr-Jawad <jawadbuniri2@gmail.com>
2025-11-29 20:51:41 +0500

    age classification

---
+++ age_classification.py
@@ -0,0 +1,9 @@
+age=int(input("enter your age : "))
+if age > 0 and age < 13 :
+    print("child")
+elif age >=13 and age < 20 :
+    print("teenage")
+elif age >= 20 and age < 65 :
+    print("adult")
+else :
+    print("senior")
