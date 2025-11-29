f442e23c31f9fb0c00674f0e81ba8b911a3bf80a
Engr-Jawad <jawadbuniri2@gmail.com>
2025-11-29 21:24:48 +0500

    fruit program

---
+++ froot_fresh.py
@@ -0,0 +1,10 @@
+fruit = "banana"
+color=input("enter the color of the fruit : ")
+if  fruit == "banana" and color == "green":
+    print("the fruit is unripe ")
+elif fruit == "banana" and color == "yellow":
+    print("the fruit is ripe")
+elif fruit == "banana" and color == "brown":
+    print ("the fruit is overrripe")
+else:
+    print("the fruit is not a banana")
