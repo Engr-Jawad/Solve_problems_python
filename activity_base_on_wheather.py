26a5ed9c74204e423d983425e026beb5d98f5256
Engr-Jawad <jawadbuniri2@gmail.com>
2025-11-29 21:32:12 +0500

    wheather app

---
+++ activity_base_on_wheather.py
@@ -0,0 +1,13 @@
+print("activit suggested based on wheather condition ")
+print("1.sunny\n2.rainy\n3.cloudy\n4.snowy")
+wheather=input("enter the wheather : ")
+if wheather == "sunny":
+    print("come dude lets go for a walk")
+elif wheather == "rainy":
+    print("lets watch a movier")
+elif wheather == "cloudy":
+    print("come lets play football dude")
+elif wheather == "snowy":
+    print("come lets build a snowman")
+else:
+    print("invalid wheahther")
