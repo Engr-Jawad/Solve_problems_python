d49644ad4b5f40d3582b011bff0e18774fb32482
Engr-Jawad <jawadbuniri2@gmail.com>
2025-12-02 20:26:40 +0500

    validate_input

---
+++ validate_input.py
@@ -0,0 +1,9 @@
+
+while True:
+    number=int(input("enter the number between one and ten : "))
+    if 1 <= number <= 10:
+        print("thank you")
+        break
+    else:
+        print("try again")
+        
