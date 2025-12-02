e7f68a6de33c939c0e491f9fd6199816af75ffc1
Engr-Jawad <jawadbuniri2@gmail.com>
2025-12-02 20:03:11 +0500

    factorial of a number

---
+++ factorial_num.py
@@ -0,0 +1,7 @@
+number=int(input("enter the number :"))
+fact = 1
+for i in  range(number):
+    fact = number * fact 
+    number = number -1
+print("the factorial of number is equal to : ",fact)
+     
